2b44cbc21d5abc39bdc6bcae2f00424e40fdf04f
Ornella <ornella@Ornellas-MacBook-Pro.local>
2024-08-02 21:12:17 +0200

    user deletion with token authentication

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -11,3 +11,29 @@ export const getUsers = async (req: Request, res: Response) => {
         return res.status(500).json({ error: 'Internal server error' });
     }
 };
+
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error('Error details:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await prisma.user.delete({ where: { id: Number(id) } });
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error('Error details:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { getUsers } from '../controllers/userController';
+import { getUsers, deleteUser, getUserById } from '../controllers/userController';
+import { authenticateUser } from '../middleware/auth';
 
 const router = Router();
 
+router.get('/user/:id', getUserById);
 router.get('/users', getUsers);
+router.delete('/user/:id', authenticateUser, deleteUser);
 
 export default router;
